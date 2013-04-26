df3844f13ed026e5ce342af4ef84615d56d2c044
Derek Dohler <derek@transparency.ge>
2013-04-26 13:30:47 +0400

    Moved load scripts into app folders.

---
+++ apps/person/load/people.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import json
+from apps.person.models import Person
+import codecs
+
+def load_relations(infile):
+    """ Loads a set of Person-Corp relations, adding relations and
+    People objects as necessary."""
+    pass
+
+def parse_person(person_json):
+    """ Takes a json string and converts it into a Person object."""
+    pass

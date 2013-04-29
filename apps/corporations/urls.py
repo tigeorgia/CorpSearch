40dfe9815b10303db1b52a2ab43eefe14f9fb1aa
Derek Dohler <derek@transparency.ge>
2013-04-29 16:12:02 +0400

    Added corporation search.

--- apps/corporations/urls.py
+++ apps/corporations/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from .views import CorporationListView, CorporationDetailView
+from .views import CorporationListView, CorporationSearchView, CorporationDetailView
 
 urlpatterns = patterns('',
     url(r'^$', CorporationListView.as_view(), name='corporation-list'),
     url(r'^(?P<pk>\d+)/$', CorporationDetailView.as_view(), name='corporation-detail'),
+    url(r'^search/$', CorporationSearchView.as_view(), name='corporation-search'),
 )
--- apps/corporations/views.py
+++ apps/corporations/views.py
@@ -10,6 +10,15 @@ class CorporationListView(ListView):
     context_object_name = 'corporations'
     template_name = 'corporations/corporation_list.html'
 
+class CorporationSearchView(CorporationListView):
+    def get_queryset(self):
+        qs = Corporation.objects.all()
+        if self.request.GET.get('id_code'):
+            qs = qs.filter(id_code=self.request.GET.get('id_code'))
+        if self.request.GET.get('name'):
+            qs = qs.filter(name__icontains=self.request.GET.get('name'))
+        return qs
+
 class CorporationDetailView(DetailView):
     model = Corporation
     context_object_name = 'corporation'

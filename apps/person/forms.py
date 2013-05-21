55ac56875d5efe95f1cbf6df908b0c072d0e7dd8
Derek Dohler <derek@transparency.ge>
2013-05-21 11:41:14 +0400

    Added nationality search to people.

--- apps/person/forms.py
+++ apps/person/forms.py
@@ -5,6 +5,7 @@ class PersonSearchForm(forms.Form):
     name = forms.CharField(max_length=200,required=False)
     personal_code = forms.CharField(max_length=50,required=False,label="Personal number")
     address = forms.CharField(max_length=200,required=False)
+    nationality = forms.CharField(max_length=200,required=False)
 
 class PersonForm(forms.ModelForm):
     class Meta:

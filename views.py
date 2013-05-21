47fce7c814790115b4dd12d793ad1d06000adb78
Derek Dohler <derek@transparency.ge>
2013-05-21 11:42:21 +0400

    Added request context to homepage.

--- views.py
+++ views.py
@@ -1,10 +1,12 @@
 from apps.corporations.forms import CorporationSearchForm
 from apps.person.forms import PersonSearchForm
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def home(request):
     corpsearch = CorporationSearchForm()
     personsearch = PersonSearchForm()
-    return render_to_response('home.html', 
-                                {'corpsearch': corpsearch,
-                                 'personsearch': personsearch,})
+    return render_to_response('home.html',
+                              {'corpsearch': corpsearch,
+                               'personsearch': personsearch,},
+                              context_instance=RequestContext(request),)

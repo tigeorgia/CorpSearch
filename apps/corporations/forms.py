d67091d35018f70a698b8a23ee51bae578f5aa7c
Etienne Baque <etiennebaque@gmail.com>
2014-02-26 13:00:51 +0400

    Translated form labels, on home page

--- apps/corporations/forms.py
+++ apps/corporations/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Corporation, Extract
 
 class CorporationSearchForm(forms.Form):
-    name = forms.CharField(max_length=250, required=False)
-    id_code = forms.CharField(max_length=50, required=False)
-    address = forms.CharField(max_length=250, required=False)
-    email = forms.CharField(max_length=250, required=False)
+    name = forms.CharField(max_length=250, required=False, label=_('Name'))
+    id_code = forms.CharField(max_length=50, required=False, label=_('ID code'))
+    address = forms.CharField(max_length=250, required=False, label=_('Address'))
+    email = forms.CharField(max_length=250, required=False, label=_('Email'))
 
 class CorporationForm(ModelForm):
     class Meta:
--- apps/person/forms.py
+++ apps/person/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from .models import Person, Affiliation
+from django.utils.translation import ugettext_lazy as _
 
 class PersonSearchForm(forms.Form):
-    name = forms.CharField(max_length=200,required=False)
-    personal_code = forms.CharField(max_length=50,required=False,label="Personal number")
-    address = forms.CharField(max_length=200,required=False)
-    nationality = forms.CharField(max_length=200,required=False)
+    name = forms.CharField(max_length=200,required=False,label=_('Name'))
+    personal_code = forms.CharField(max_length=50,required=False,label=_("Personal number"))
+    address = forms.CharField(max_length=200,required=False,label=_("Address"))
+    nationality = forms.CharField(max_length=200,required=False,label=_("Nationality"))
 
 class PersonForm(forms.ModelForm):
     class Meta:

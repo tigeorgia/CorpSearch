67f7d868c6a6627783f7a6ccc513432e4ed2a99c
GeorgeLubaretsi <gio@transparency.ge>
2014-03-12 17:09:42 +0400

    Redirect old pk urls to id_code urls

--- apps/corporations/urls.py
+++ apps/corporations/urls.py
@@ -5,7 +5,8 @@ urlpatterns = patterns('',
     # This will show every corporation on one page. Since there are now 150,000+
     # corporations, this is not recommended.
     #url(r'^$', CorporationListView.as_view(template_name="corporations/corporation_list.html"), name='corporation-list'),
-    url(r'^(?P<pk>\d+)/$', CorporationDetailView.as_view(template_name="corporations/detail.html"), name='corporation-detail'),
+
+    url(r'^(?P<id_code>\d+)/$', CorporationDetailView.as_view(template_name="corporations/detail.html"), name='corporation-detail'),
     url(r'^search/$', CorporationPagedTemplateSearchView.as_view(template_name="corporations/corporation_list.html"), name='corporation-search'),
     url(r'^search/csv/$', CorporationCsvSearchView.as_view(), name='corporation-search-csv'),
 )
--- apps/corporations/views.py
+++ apps/corporations/views.py
@@ -4,9 +4,10 @@ from .models import Corporation, LegalFormLookup
 from .forms import CorporationSearchForm
 from apps.util.views import CsvResponseMixin
 
+from django.http import HttpResponsePermanentRedirect
 from django.views.generic.list import ListView, BaseListView, MultipleObjectTemplateResponseMixin
 from django.views.generic.detail import DetailView
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from django.shortcuts import get_object_or_404
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -48,7 +49,7 @@ class CorporationSearchView(CorporationListView):
         companiesRegisteredAfter = self.request.GET['companies_registered_after_0']
         if companiesRegisteredAfter:
             if len(companiesRegisteredAfter) == 4:
-                companiesRegisteredAfter = companiesRegisteredAfter + "-01-01";
+                companiesRegisteredAfter = companiesRegisteredAfter+"-01-01";
 
             if (len(companiesRegisteredAfter) == 10 or len(companiesRegisteredAfter) == 4):
                 qs = qs.filter(registration_date__gte=companiesRegisteredAfter)
@@ -56,7 +57,7 @@ class CorporationSearchView(CorporationListView):
         companiesRegisteredBefore = self.request.GET['companies_registered_before_0']
         if companiesRegisteredBefore:
             if len(companiesRegisteredBefore) == 4:
-                companiesRegisteredBefore = companiesRegisteredBefore + "-01-01";
+                companiesRegisteredBefore = companiesRegisteredBefore+"-01-01";
 
             if (len(companiesRegisteredBefore) == 10 or len(companiesRegisteredBefore) == 4):
                 qs = qs.filter(registration_date__lte=companiesRegisteredBefore)
@@ -77,5 +78,27 @@ class CorporationDetailView(DetailView):
     context_object_name = 'corporation'
 
     def get_object(self, queryset=None):
-        return get_object_or_404(Corporation, id_code=self.kwargs['pk'])
+        #check if object is already cached
+        if not getattr(self, 'object', None):
+            #id_code of corporation seems to be 9 characters long, all other id's are pk
+            if len(self.kwargs['id_code']) < 9:
+                obj = get_object_or_404(Corporation, pk=self.kwargs['id_code'])
+            else:
+                obj = get_object_or_404(Corporation, id_code=self.kwargs['id_code'])
+            return obj
 
+    def get(self, request, *args, **kwargs):
+        """
+        Override main get method in order to preserve old url structure, where corporations were accessed by pk
+        and not by id_code. This is needed until we import next batch of scraped data, pks will be replaced anyway.
+        But by that time, Google will probably already detect 301 redirects.
+
+        @todo: Remove this override when we run the next batch of import
+        """
+        self.object = self.get_object()
+        obj_real_url = self.object.get_absolute_url()
+        if self.request.path != obj_real_url:
+            return HttpResponsePermanentRedirect(obj_real_url)
+        else:
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)

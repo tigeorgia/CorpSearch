2d22b832d6707b0a92be9ee95ba0d4e972dd5796
Marek Suliga <marek.suliga@transparency.ge>
2014-12-11 09:30:36 +0400

    Added Admin page for the Person model so one can set no_index_tag.

---
+++ apps/person/admin.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 10, 2014
+
+@author: msuliga
+'''
+from django.contrib import admin
+from .models import Person
+
+class PersonAdmin( admin.ModelAdmin ):
+    search_fields = ['name', 'address']
+    list_display = ( 'name', 'address', 'no_index_tag', 'id' )
+    list_editable = ('no_index_tag',)
+        
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    
+admin.site.register( Person, PersonAdmin )

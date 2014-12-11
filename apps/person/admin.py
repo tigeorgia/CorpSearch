dbff54da22f196030fcb753f0b4da52ae42697a6
Marek Suliga <marek.suliga@transparency.ge>
2014-12-11 09:56:10 +0400

    Only no_index_tag field is editable for Peron model

--- apps/person/admin.py
+++ apps/person/admin.py
@@ -10,9 +10,12 @@ class PersonAdmin( admin.ModelAdmin ):
     search_fields = ['name']
     list_display = ( 'name', 'address', 'no_index_tag', 'id' )
     list_editable = ('no_index_tag',)
+    fields = [ 'no_index_tag']    
         
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def has_add_permission(self, request):
+        return False
     
 admin.site.register( Person, PersonAdmin )
